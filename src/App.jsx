680d872a262c7617f89d2217a7a825356b4f66cc
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2023-12-22 20:53:31 -0300

    Iniciando-Projeto

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import "./App.css";
+import Todo from './components/Todo';
+
+
+function App() {
+  const [todos, setTodos] = useState([
+    
+    {
+      id:1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ])
+
+  return( 
+  <div className="app">
+  <h1>Lista de Tarefas</h1>
+  <div className='todo-list'>
+   {/* percorrer o array de objeto e exibir todos os TODO*/}
+    {todos.map((todo) => (  
+      <Todo todo={todo}/>
+    ))}
+  </div>
+</div>)
+  
+
+}
+export default App
