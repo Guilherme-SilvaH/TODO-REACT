67501831c9839160d48747918c85c73bfdff0571
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2023-12-23 17:46:46 -0300

    ASC AND DESC

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,16 @@ function App() {
     }
   ]);
 
-  const [search, setSearch] = useState("")
 
+  /* states do projeto */ 
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
+
+
+
+/* Funcoes do projeto(obs: poderia fazer em uma outra pagina para uma boa pratica) */ 
   const addTodo = (text, category) => {
     const newTodos = [...todos, { /*vai receber todos os TODOs por spread e vai criar um novo objeto de TODOs */
       id: Math.floor(Math.random() * 10000), /* Gerar um numero aleatorio */
@@ -57,17 +65,37 @@ function App() {
     newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo); /* Filter altera o array original */ 
     setTodos(newTodos);
 
-  }
+  };
+  
 
+  /* Aqui é o que vai ser exibido na Pagina */ 
   return( 
   <div className="app">
   <h1>Lista de Tarefas</h1>
   <Search search={search} setSearch={setSearch}></Search>
-  <Filter></Filter>
+  <Filter filter={filter} setFilter={setFilter} setSort={setSort}></Filter>
   <div className='todo-list'>
    {/* percorrer o array de objeto e exibir todos os TODO*/}
-    {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()) /* faz um filtro que se o pesquisar for igual a funcao pesquisar, ele ja aplica o filtro */
+    {todos
+    .filter((todo) => 
+    filter === "All" /* Esse filtro faz com que se tiver "All" vai estar true, dps faz outro filtro que se tiver completo, retorna apenas os isComplete, se nao, retorna o que nao estiver isComplete*/
+     ? true 
+     : filter === "Completed" 
+     ? todo.isCompleted 
+     : !todo.isCompleted
+     )
+
+    .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()) /* faz um filtro que se o pesquisar for igual a funcao pesquisar, ele ja aplica o filtro */
     )
+
+    .sort( (a, b) =>   /* Este método sort recebe uma função de comparação como argumento. A função de comparação compara dois elementos, a e b, do array. */
+      sort === "Asc"  /* Verifica se a variável sort é igual a "Asc" (ascendente) */
+        ? a.text.localeCompare(b.text) /*Se sort for "Asc", usa localeCompare para comparar os textos (text) de a e b e determinar a ordem ascendente.*/
+        : b.text.localeCompare(a.text) /* b.text.localeCompare(a.text): Se sort não for "Asc" (ou seja, será "Desc"), inverte a ordem usando localeCompare de b para a.*/
+    )
+ 
+    
+
     .map((todo) => (  
       <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
     ))}
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,25 +1,24 @@
-
-function Filter() {
-  return (
-    <div className="filter">
+function Filter({ filter, setFilter, setSort }) {
+    return (
+      <div className="filter">
         <h2>Filtrar:</h2>
         <div className="filter-options">
-            <div>
-                <p>Status: </p>
-                <select>
-                    <option value="all">Todas</option>
-                    <option value="Completed">Completas</option>
-                    <option value="Incomplete">Incompletas</option>
-                </select>
-            </div>
-            <div>
-                <p>Orden Alfabética:</p>
-                <button>Asc</button>
-                <button>Desc</button>
-            </div>
+          <div>
+            <p>Status: </p>
+            <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+              <option value="All">Todas</option>
+              <option value="Completed">Completas</option>
+              <option value="Incompleted">Incompletas</option>
+            </select>
+          </div>
+          <div>
+            <p>Orden Alfabética:</p>
+            <button onClick={() => setSort("Asc")}>Asc</button>
+            <button onClick={() => setSort("Desc")}>Desc</button>
+          </div>
         </div>
-    </div>
-  )
-}
-
-export default Filter
+      </div>
+    );
+  }
+  
+  export default Filter;
